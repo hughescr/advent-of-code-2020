c44352e57f515cf83bbd14614983566209f3775e
Craig R. Hughes <craig.git@rungie.com>
2020-12-09 15:52:16 -0800

    Day 9 prob 2

--- 9thDay/prob2.js
+++ 9thDay/prob2.js
@@ -9,4 +9,32 @@ readFile('input.txt', 'utf8', (err, data) => {
     .split('\n')
     .map(line => parseInt(line))
     .value();
+
+    let i = 25;
+    while(i < lines.length) {
+        const current = lines[i];
+        const previous25 = _.slice(lines, i-25, i);
+        const num1 = _(previous25).findLast((n, i) => _(previous25).take(i).find(n2 => n2 === current-n));
+        if(!num1) {
+            break;
+        }
+        i++;
+    }
+    const badnum = lines[i];
+    const badposition = i;
+
+    for(let start = 0; start < badposition; start++) {
+        for(let end = start; end < badposition; end++) {
+            const slice = _(lines).slice(start, end+1).sortBy().value();
+            const sum = _.sum(slice);
+            if(sum > badnum) {
+                break;
+            }
+
+            if(sum === badnum) {
+                console.log(slice[0] + slice[slice.length - 1]);
+                process.exit(0);
+            }
+        }
+    }
 });
